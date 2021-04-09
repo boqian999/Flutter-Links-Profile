02d4eb642ca5e1dae0d5058b08bc12f4bf06aca0
boqian-kavi <boqian.wang@kaviglobal.com>
2021-04-09 16:06:16 -0500

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'models/link_data.dart';
+
+final documents = [
+  LinkData(title: 'Youtube', url: 'https://youtube.com'),
+  LinkData(title: 'LinkedIn', url: 'https://linkedin.com'),
+  LinkData(title: 'Facebook', url: 'https://facebook.com'),
+];
+
+final imageUrl =
+    'https://pbs.twimg.com/profile_images/624447901180293120/hDir9ur7_400x400.jpg';
